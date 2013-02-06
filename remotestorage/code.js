3262889ce6100acdb8e86d5e7d3b6e95c6fa7a99
Michiel de Jong <michiel@unhosted.org>
2013-02-06 12:48:50 +0800

    use path in goToDir and whitespace

--- remotestorage/code.js
+++ remotestorage/code.js
@@ -3,14 +3,16 @@ remoteStorage.defineModule('code', function(privateClient, publicClient) {
     dirCb = function() {},
     currDir, currFile;
   function goToDir(path) {
-    privateClient.getListing(path).then(function(arr) {
-      currDir = path;
-      var dirParts = path.split('/'),
-	obj = { items: arr, dir: [] };
-      for(var i=0; i<dirParts.length-1; i++) {
-	obj.dir.push(dirParts[i]+'/');
-      }
-      dirCb(obj);
+    privateClient.use(path).then(function() {
+      privateClient.getListing(path).then(function(arr) {
+        currDir = path;
+        var dirParts = path.split('/'),
+        obj = { items: arr, dir: [] };
+        for(var i=0; i<dirParts.length-1; i++) {
+          obj.dir.push(dirParts[i]+'/');
+        }
+        dirCb(obj);
+      }); 
     }); 
   }
   function goToFile(path) {
@@ -32,23 +34,23 @@ remoteStorage.defineModule('code', function(privateClient, publicClient) {
   return {
     exports: {
       init: function() {
-	privateClient.release('');
-	publicClient.release('');
+        privateClient.release('');
+        publicClient.release('');
       },
       up: function(path, type, content) {
-	console.log('privateClient.storeFile', type, path, content);
-	return privateClient.storeFile(type, path, content);
+        console.log('privateClient.storeFile', type, path, content);
+        return privateClient.storeFile(type, path, content);
       },
       down: function(path) {
-	privateClient.getFile(path).then(function(obj) {
-	  obj.path = path;
-	  currCb(obj);
-	});
+        privateClient.getFile(path).then(function(obj) {
+          obj.path = path;
+          currCb(obj);
+        });
       },
       goToDir: goToDir,
       goToFile: goToFile,
       onDir: function(cb) {
-	dirCb = cb;
+        dirCb = cb;
       },
       onCurr: function(cb) {
         currCb = cb;
